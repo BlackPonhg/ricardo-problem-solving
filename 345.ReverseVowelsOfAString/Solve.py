4dbbf4024c469901fb1fbd2b5d2f568b7a56d83a
Ricardo Garcia <ricardogarciacabral@gmail.com>
2026-03-12 21:20:05 -0400

    Solucion problema 345

---
+++ 345.ReverseVowelsOfAString/Solve.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowels = set("aeiouAEIOU")
+        chars = list(s)
+
+        i, j = 0, len(chars) - 1
+        while i < j:
+            while i < j and chars[i] not in vowels:
+                i += 1
+            while i < j and chars[j] not in vowels:
+                j -= 1
+
+            chars[i], chars[j] = chars[j], chars[i]
+            i += 1
+            j -= 1
+
+        return "".join(chars)
+        
+        
