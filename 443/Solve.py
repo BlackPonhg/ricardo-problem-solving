212f9c94a93ea42a90bb53bbe1b0ec9cd2137e15
Ricardo Garcia <ricardogarciacabral@gmail.com>
2026-03-19 20:58:40 -0400

    Solucion al problema 443

---
+++ 443/Solve.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        n = len(chars)
+        write = 0
+        read = 0
+
+        while read < n:
+            ch = chars[read]
+            start = read
+
+            while read < n and chars[read] == ch:
+                read += 1
+            count = read - start
+
+            chars[write] = ch
+            write += 1
+
+            if count > 1:
+                for digit in str(count):
+                    chars[write] = digit
+                    write += 1
+
+        return write
